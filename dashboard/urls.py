bbb772a106005b8905f9e6c691ab883f8d040050
marcosColo <marcosalvarez401@gmail.com>
2026-04-13 23:56:17 -0300

    Cambie la disposiciones de las tarjetas en la pagina principal, organice e hice varios comentarios para comprender todas las vistas que hay y que estabas mezcladas haciendo mas legible la logica de la web.

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,11 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard),
-    path('formIngreso/', views.formIng, name='nuevo_Ing'),
-    path('nuevoIngreso/', views.nuevoIngreso),
-    path('formGasto/', views.formGas, name='nuevo_Gas'),
-    path('nuevoGasto/', views.nuevoGasto),
-    path('eliminar/<id>', views.eliminarIng),
-    path('eliminargas/<id>', views.eliminarGas),
+    path('', views.dashboard, name='inicio'),                       # Pagina principal
+    path('formIngreso/', views.formIng, name='nuevo_Ing'),          # Form para registrar nuevo ingreso
+    path('formRegIngreso/', views.formRegIng, name='formRegIng'),   # Form para registrar nuevo ingreso dentro del registro de ingresos
+    path('nuevoRegIngreso/', views.nuevoRegIngreso),  
+    path('nuevoIngreso/', views.nuevoIngreso),                      # Logica para nuevo ingreso
+    path('formGasto/', views.formGas, name='nuevo_Gas'),            # Form para registrar nuevo gasto
+    path('nuevoGasto/', views.nuevoGasto),                          # Logica para nuevo gasto
+    path('eliminar/<id>', views.eliminarIng),                       # Logica para eliminar ingreso
+    path('eliminargas/<id>', views.eliminarGas),                    # Logica para eliminar gasto
+    path('registroIngreso', views.regIng, name='regIng'),           # Pagina con todos los ingresos
+    path('registroGasto', views.regGas, name='regGas'),             # Pagina con todos los gastos
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render, redirect
 from .models import ingresos, gastos, balance
 
 # Create your views here.
+
+# VIEWS
+# VIEWS
+# VIEWS
+# VIEWS
+
+# View de la pagina principal del dashboard
+# =====================================================
 def dashboard(request):
     dataIng = ingresos.objects.all()
     dataGas = gastos.objects.all()
@@ -27,21 +35,96 @@ def dashboard(request):
 
     return render(request, 'dashboard.html', {'contexto':contexto})
 
+# =====================================================
+
+# FORMULARIOS
+# =====================================================
+# Para registar nuevo ingreso
 def formIng(request):
     return render (request, 'formIng.html')
 
+# Para registrar nuevo gasto
+def formGas(request):
+    return render (request, 'formGas.html')
+
+# =====================================================
+
+# Views de los registros completos de ingresos y gastos
+# =====================================================
+# Ingresos
+def regIng(request):
+    dataIng = ingresos.objects.all()
+    balact = ingresos.objects.values_list('monto', flat=True)
+    
+    balAct = 0
+
+    for i in balact:
+        balAct = balAct + i
+
+    contexto = {
+        'a':dataIng,
+        'b':balAct
+    }
+
+    return render(request, 'regIng.html', {'contexto':contexto})
+
+# Para registar nuevo ingreso dentro del registro de ingresos
+def formRegIng(request):
+    return render (request, 'formRegIng.html')
+
+# Gastos
+def regGas(request):
+    dataGas = gastos.objects.all()
+    balpas = gastos.objects.values_list('monto', flat=True)
+    
+    balPas = 0
+
+    for i in balpas:
+        balPas = balPas + i
+
+    contexto = {
+        'b':dataGas,
+
+    }
+
+    return render(request, 'regGas.html', {'contexto':contexto})
+
+# Boton de eliminar dentro del registro de ingresos
+def elimiRegIng(request, id):
+    registro = ingresos.objects.get(id=id)
+    registro.delete()
+
+    return redirect('regIng.html')
+
+# Boton de eliminar dentro del registro de gastos
+def elimiRegGas(request, id):
+    registro = gastos.objects.get(id=id)
+    registro.delete()
+
+    return redirect('/')
+# =====================================================
+# =====================================================
+
+#   LOGICA
+#   LOGICA
+#   LOGICA
+#   LOGICA
+# =====================================================
+# Para eliminar un ingreso ya creado (la cruz que esta en cada card)
 def eliminarIng(request, id):
     registro = ingresos.objects.get(id=id)
     registro.delete()
 
     return redirect('/')
 
+# Para eliminar un gasto ya creado
 def eliminarGas(request, id):
     registro = gastos.objects.get(id=id)
     registro.delete()
 
     return redirect('/')
 
+# Logica para crear un nuevo registro (objeto) de ingreso (instancia la clase (modelo) ingresos)
 def nuevoIngreso(request):
     detalle = request.POST.get('ingreso-Detalle', False)
     monto = request.POST.get('ingMonto', False)
@@ -49,12 +132,20 @@ def nuevoIngreso(request):
     nuevo_ingreso = ingresos.objects.create(detalle=detalle, monto=monto)
     return redirect('/')
 
-def formGas(request):
-    return render (request, 'formGas.html')
+# Logica para crear un nuevo ingreso DENTRO de la pagina donde estan todos los registros de ingreso
+def nuevoRegIngreso(request):
+    detalle = request.POST.get('ingreso-Detalle', False)
+    monto = request.POST.get('ingMonto', False)
 
+    nuevo_ingreso = ingresos.objects.create(detalle=detalle, monto=monto)
+    return redirect('registroIngreso/')
+
+# Logica para crear un nuevo registro de gasto (instancia a gastos)
 def nuevoGasto(request):
     detalle = request.POST.get('gasto-Detalle', False)
     monto = request.POST.get('gasMonto', False)
 
     nuevo_ingreso = gastos.objects.create(detalle=detalle, monto=monto)
     return redirect('/')
+# =====================================================
+
